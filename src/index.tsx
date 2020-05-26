87fb4281dcf5228088a4d32ec2dcbe210ab93b08
vladinomo <reasonistreason.t.rex@gmail.com>
2020-05-26 23:42:54 +0900

    eslint config for react

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => <div>hello!</div>
+const App = (): JSX.Element => <div>hello!</div>
 
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.querySelector('#root'))
